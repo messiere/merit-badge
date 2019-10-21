45c990e6709b9b6c2d6eb20db268c38345f712dc
messiere <54910561+messiere@users.noreply.github.com>
2019-10-21 14:15:26 -0400

    First commit

---
+++ guess/marit.js
@@ -0,0 +1,11 @@
+var anw = Math.floor(Math.random()*100)+1;
+var guess = "0"
+while(guess!=anw){
+  guess=prompt("Guess a number between(1-100)");
+  if(guess<anw) alert(guess+" is to low");
+  else if(guess>anw) alert(guess+" is to high");
+  else if(guess=="q") break;
+  else{}
+}
+if(guess==anw) alert("correct");
+else alert("Cheater, the number was "+anw);
